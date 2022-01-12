05f6d5a918a6006fb938ba46d102036937742758
Giancarlo <giancarlorondo@outlook.com>
2022-01-12 11:40:12 +0100

    obtencion de datos del backend

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
     Button,
@@ -12,64 +14,116 @@ import {
 } from 'reactstrap';
 import './Home.scss';
 
-const Home = () => {
-    // Modal open state
-    const [modal, setModal] = React.useState(false);
+const Home = (props) => {
 
-    // Toggle for Modal
-    const toggle = () => setModal(!modal);
+    let history = useHistory();
+    const [books, setBooks] = useState([]);
+    const [book, setBook] = useState({});
+    const [infobook, setInfoBook] = useState({});
 
-    return (
-        <div className="home">
-            <div className="book">
+    //obtengo datos
+    useEffect(() => {
+        const getBooks = async () => {
+            let result = await axios.get(`http://localhost:3001/books`);
+
+            setBooks(result.data);
+        }
+        getBooks();
+    }, []);
+
+    const editBook = book => {
+        setBook(book);
+        toggle();
+    };
+
+    const dataBook = infobook => {
+        setInfoBook(infobook);
+        toggleInfo();
+    }
+
+    const getBooksElements = () => {
+        return books.map((book) => {
+            return <div className="book">
                 <Card
                     color="light"
                     outline
                     body
-                    size='sm'
-                >
+                    size='sm'>
                     <CardBody>
-                        <CardTitle tag='h5'>
-                            BOOK 1
+                        <CardTitle tag='h5' onClick={() => dataBook(book)}>
+                            {book.name}
                         </CardTitle>
 
                         <Button
                             color="success"
                             outline
                             size="sm"
-                            onClick={toggle}
+                            onClick={() => editBook(book)}
                         >
                             Edit
                         </Button>
-
-                        <Modal
-                            centered
-                            fullscreen="md"
-                            scrollable
-                            size="lg"
-                            isOpen={modal}
-                            toggle={toggle}
-                            modalTransition={{ timeout: 1000 }}
-                        >
-                            <ModalHeader toggle={toggle}>
-                                <Input
-                                    id="title"
-                                    name="title"
-                                    placeholder="Book1"
-                                    type="text" />
-                            </ModalHeader>
-                            <ModalBody>
-                                <Input
-                                    id="description"
-                                    name="description"
-                                    placeholder="frferfrfrferfreferfrf"
-                                    type="textarea"
-                                />
-                            </ModalBody>
-                        </Modal>
                     </CardBody>
                 </Card>
             </div>
+        })
+    }
+
+    // Modal open state
+    const [modal, setModal] = React.useState(false);
+    // Modal open state
+    const [modalInfo, setModalInfo] = React.useState(false);
+
+    // Toggle for Modal
+    const toggle = () => setModal(!modal);
+    // Toggle for Modal
+    const toggleInfo = () => setModalInfo(!modalInfo);
+
+    return (
+        <div className="home">
+            <Modal
+                centered
+                fullscreen="md"
+                scrollable
+                size="lg"
+                isOpen={modal}
+                toggle={toggle}
+                modalTransition={{ timeout: 1000 }}
+            >
+                <ModalHeader toggle={toggle}>
+                    <Input
+                        id="title"
+                        name="title"
+                        placeholder={book.name}
+                        type="text" />
+                </ModalHeader>
+                <ModalBody>
+                    <Input
+                        id="description"
+                        name="description"
+                        placeholder={book.description}
+                        type="textarea"
+                    />
+                </ModalBody>
+            </Modal>
+
+            <Modal
+                centered
+                fullscreen="md"
+                scrollable
+                size="lg"
+                isOpen={modalInfo}
+                toggle={toggleInfo}
+                modalTransition={{ timeout: 1000 }}
+            >
+                <ModalHeader toggle={toggleInfo}>
+                    {infobook.name}
+                </ModalHeader>
+                <ModalBody>
+                    {infobook.description}
+                </ModalBody>
+            </Modal>
+            
+            {getBooksElements()}
         </div>
     )
 }
